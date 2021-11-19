3c73f29b1faa139559657f1e1d0237bce6dcf669
Joel-C-P <joelcpd77@gmail.com>
2021-11-19 13:37:32 +0100

    Creación de muros, y creación de teletransporte al otro lado del muro

--- M05_UF1_1o_P_42_EvaCanasHernandez_JoelCallapaPrado/Main.cpp
+++ M05_UF1_1o_P_42_EvaCanasHernandez_JoelCallapaPrado/Main.cpp
@@ -34,3 +34,31 @@ int main()
 	}
 
 }
+
+void RellenarMapa()
+{
+	//Valores y posiciones que se dan para la fila y la columna
+	for (int i = 0; i < CONSOLE_HEIGHT; i++)
+	{
+		for (int j = 0; j < CONSOLE_WIDTH; j++)
+		{
+			//cout << i << "," << j << ".";
+			if (i == 0 || i == CONSOLE_HEIGHT - 1 || j == 0 || j == CONSOLE_WIDTH - 1)
+			{
+				ConsoleScreen[i][j] = MAP_TILES::WALL;
+			}
+			else
+			{
+				ConsoleScreen[i][j] = MAP_TILES::EMPTY;
+			}
+
+		}
+
+	}
+
+
+
+
+
+
+}
