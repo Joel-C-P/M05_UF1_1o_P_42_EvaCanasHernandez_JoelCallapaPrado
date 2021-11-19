76d2cfce3547c24ec4d57d8dfb852fc6e572ba47
Joel-C-P <joelcpd77@gmail.com>
2021-11-19 13:20:55 +0100

    Posición inicial del personaje

--- M05_UF1_1o_P_42_EvaCanasHernandez_JoelCallapaPrado/Main.cpp
+++ M05_UF1_1o_P_42_EvaCanasHernandez_JoelCallapaPrado/Main.cpp
@@ -15,3 +15,11 @@ enum USER_INPUTS { NONE, UP, RIGHT, QUIT, LEFT, DOWN };
 
 //Caractares para imprimir en consola
 MAP_TILES ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDTH];
+
+
+char personaje = 'O';
+//Posicion inicial del jguador en consola
+int personaje_x = 10;
+int personaje_y = 5;
+int personaje_points = 0;
+int map_points = 0;
